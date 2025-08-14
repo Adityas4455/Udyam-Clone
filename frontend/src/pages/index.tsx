82fc05299d61e3f1597ed79304837a2ed9b86a2c
Adityas4455 <siddhiaditya01@gmail.com>
2025-08-15 01:21:06 +0530

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import TwoStepForm from "./components/TwoStepForm";
+import TwoStepForm from "./components/TwoStepForm.tsx";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
